17729400603e81bc389051c9eeb40d72d844888a
Cristian <Kristian2964@gmail.com>
2026-05-13 10:21:41 -0500

    Segunda version: ventana como clase

--- practica_pyqt_sqlite/main.py
+++ practica_pyqt_sqlite/main.py
@@ -5,12 +5,17 @@ from PyQt6.QtWidgets import (
     QWidget
 )
 
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Registro de Usuarios")  #Define el título de la ventana
+        self.setGeometry(100, 100, 300, 150)         #Define la posición (x, y) y el tamaño (ancho, alto) de la ventana
+
+
 app = QApplication(sys.argv)
 
-ventana = QWidget()                             #Crea una ventana básica.
-ventana.setWindowTitle("Registro de Usuarios")  #Define el título de la ventana
-ventana.setGeometry(100, 100, 300, 150)         #Define la posición (x, y) y el tamaño (ancho, alto) de la ventana
-ventana.show()                                  #Muestra la ventana en la pantalla
+window = MainWindow()                             #Crea una ventana básica.
+window.show()                                  #Muestra la ventana en la pantalla
 
 sys.exit(app.exec())                            #Inicia el bucle de eventos de la aplicación.
 
